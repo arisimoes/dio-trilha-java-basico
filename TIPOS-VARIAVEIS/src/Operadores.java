204b550b7b4fe52bbfe9065bab6d1f1fa8b22d8c
Ari Simões <arisimoes@gmail.com>
2024-09-03 20:42:38 -0300

    operadores

---
+++ TIPOS-VARIAVEIS/src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+    public static void main(String[] args) {
+       //repetição numerica de incrementação
+       int numero = 5;
+       numero = numero + 2;
+       numero ++;
+       System.out.println(++ numero );
+       System.out.println(numero);
+    }
+}
