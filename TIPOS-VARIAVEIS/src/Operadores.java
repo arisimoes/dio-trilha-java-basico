aaf05a4c93e4bcf018bd03ee553f9ce4804f0404
Ari Simões <arisimoes@gmail.com>
2024-09-04 11:58:47 -0300

    opeeradores

--- TIPOS-VARIAVEIS/src/Operadores.java
+++ TIPOS-VARIAVEIS/src/Operadores.java
@@ -11,5 +11,28 @@ public class Operadores {
        System.out.println(variavel);
        variavel = !variavel;
        System.out.println(variavel);
+
+
+
+       int a, b;
+
+       a = 5;
+       b = 6;
+
+      // String resultado = "";
+
+       /*if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+        
+        System.out.println(resultado);
+
+        //operador ternario
+         */
+        String resultado = a!=b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+
+
     }
 }
